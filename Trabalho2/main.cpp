0da1500185cff90da662ba044a7e181bfffbc397
calebe-fcs <cfcs317@gmail.com>
2025-05-17 18:00:05 -0300

    Add files via upload

---
+++ Trabalho2/main.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<cstring>
+#include<locale>
+
+using namespace std;
+
+int main()
+
+
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    cout << ("Sou aluno de BSI no IFSP\n");
+    return 0;
+}

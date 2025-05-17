2ae912a2d84d635869be9efa6a977c109f209549
calebe-fcs <cfcs317@gmail.com>
2025-05-17 18:11:41 -0300

    Update and rename main.cpp to Exercicio14.cpp

--- Trabalho2/Exercicio14.cpp
+++ Trabalho2/Exercicio14.cpp
@@ -16,11 +16,11 @@ int main()
 
         if (peso > 50)
         {
-            cout << (" Você excedeu a carga limite e deverá pagar multa! \n");
+            cout << (" VocÃª excedeu a carga limite e deverÃ¡ pagar multa! \n");
             excesso = peso -50;
             multa = excesso/50;
             valor_m = multa * 4;
-            cout << " Você deverá pagar " << valor_m << " reais devido ao excesso de " << excesso << " kilos.";
+            cout << " VocÃª deverÃ¡ pagar " << valor_m << " reais devido ao excesso de " << excesso << " kilos.";
         }
 
 

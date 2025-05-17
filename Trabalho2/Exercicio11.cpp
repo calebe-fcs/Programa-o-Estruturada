364b02fe46a3f328f0c65ef4ef290f4d1c5e656c
calebe-fcs <cfcs317@gmail.com>
2025-05-17 18:09:37 -0300

    Update and rename main.cpp to Exercicio11.cpp

--- Trabalho2/Exercicio11.cpp
+++ Trabalho2/Exercicio11.cpp
@@ -10,12 +10,12 @@ int main()
     float salario_b, salario_l;
     const float ir = 7.0/100.0;
 
-    cout << (" Olá, digite o quanto você ganha de salário bruto: \n");
+    cout << (" OlÃ¡, digite o quanto vocÃª ganha de salÃ¡rio bruto: \n");
     cin >> salario_b;
 
     salario_l = salario_b - ( salario_b * ir);
 
-    cout << " Seu salário bruto é de: " << salario_b << " reais, porem retirando o imposto de renda (7%), \n seu salário líquido passa a ser " << salario_l << " reais. \n";
+    cout << " Seu salÃ¡rio bruto Ã© de: " << salario_b << " reais, porem retirando o imposto de renda (7%), \n seu salÃ¡rio lÃ­quido passa a ser " << salario_l << " reais. \n";
 
 
     return 0;
